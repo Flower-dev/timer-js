36afbaec5330f4bb9cc18529032e77d7895a859a
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2023-01-31 18:39:35 +0100

    init

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const start = 10;
 let dataTime = start * 60;
 
-const timerElement = document.getElementById("timer")
+const newStart = 10;
+let newDataTime = newStart * 60;
 
+const timerElement = document.getElementById("timer")
+const newTimer = document.getElementById("timerAdd")
 
 const decrement = () => {
     let min = parseInt(dataTime / 60, 10);
@@ -14,5 +17,17 @@ const decrement = () => {
     timerElement.innerText = `${min}:${sec}`
     dataTime = dataTime <= 0 ? 0 : dataTime - 1
 }
-
 setInterval(decrement, 1000)
+
+const increment = () => {
+    let min = parseInt(newDataTime / 60, 10);
+    let sec = parseInt(newDataTime % 60, 10)
+    
+    min = min < 10 ? "0" + min : min
+    sec = sec < 10 ? "0" + sec : sec
+
+    newTimer.innerText = `${min}:${sec}`
+    newDataTime = newDataTime <= 0 ? 0 : newDataTime + 1
+}
+
+setInterval(increment, 1000)
